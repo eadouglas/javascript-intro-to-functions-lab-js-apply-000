5f193e62b34e345eff8dd1376cff0aa984e1921b
eustace douglas <eadouglas@live.co.uk>
2018-09-17 20:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ function shout(string){
     console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-    if (string === string.toLowerCase()) 
+    if (string === string.toLowerCase()) {
         return "I can't hear you!"
     }
-    
-function sayHiToGrandma(string){
-      if (string === string.toUpperCase()) 
+    if (string === string.toUpperCase()) {
         return "YES INDEED!"
     }
+    if (string === "I love you, Grandma.") {
+        return "I love you, too."
+    }
+}
